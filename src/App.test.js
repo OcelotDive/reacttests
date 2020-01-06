441f6d109266e8d55d4c4f730d38db7002d72c92
ocelotdive <oscar1dlh@hotmail.com>
2020-01-06 13:20:29 +0000

    test: total

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import {add} from './App';
+import {add, total} from './App';
 
 test('add', () => {
   const value = add(2,3);
   expect(value).toEqual(5);
   expect(value).toBeLessThan(6)
   
+})
+
+
+test('total', () => {
+  expect(total(5, 20)).toBe("$ 25");
 })
